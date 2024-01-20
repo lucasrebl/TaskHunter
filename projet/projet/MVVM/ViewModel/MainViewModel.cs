2695dd6880ec931080acba7a075856a384c2ed5a
Bechamelloww <gaby@youmail.fr>
2024-01-20 17:24:00 +0100

    adding final designed window, began dashboard

---
+++ projet/projet/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using projet.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand DashboardViewCommand { get; set; }
+        public RelayCommand GameViewCommand { get; set; }
+
+        public DashboardViewModel DashboardVM {  get; set; }
+        public GameViewModel GameVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChange();
+            }
+        }
+
+
+
+
+
+        public MainViewModel()
+        {
+            DashboardVM = new DashboardViewModel();
+            GameVM = new GameViewModel();
+
+            CurrentView = DashboardVM;
+
+            DashboardViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DashboardVM;
+            });
+
+            GameViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = GameVM;
+            });
+        }
+    }
+}
