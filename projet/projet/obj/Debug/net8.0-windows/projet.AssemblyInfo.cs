e5b00e6581fa747a6c04cb50abf6b816ecfcd7d0
Bechamelloww <gabriel.marie@ynov.com>
2024-01-23 17:02:26 +0100

    pull tasks

--- projet/projet/obj/Debug/net8.0-windows/projet.AssemblyInfo.cs
+++ projet/projet/obj/Debug/net8.0-windows/projet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e24a7ee5d15e24b870f97a62b9c9c956cbc23984")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c480b4a4fc140f937475a364755c174b85abeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("projet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
