08cac616591120d3b11160f924bc20fba40cb389
Bechamelloww <gabriel.marie@ynov.com>
2024-01-31 11:15:41 +0100

    aadae

--- projet/projet/obj/Debug/net8.0-windows/projet.AssemblyInfo.cs
+++ projet/projet/obj/Debug/net8.0-windows/projet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f292f1ac238a8069c010b0d4adb92a9de85d83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae87e3c49c199dd224d172e8baaecbd7fe1faa45")]
 [assembly: System.Reflection.AssemblyProductAttribute("projet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
